6d2c5e3d72a0e36c2a8ce3769303e3cded60ec0c
jwhowes <howesjacob8@gmail.com>
2024-12-13 16:50:25 +0000

    Made discriminator optional

--- train_vae.py
+++ train_vae.py
@@ -6,7 +6,7 @@ from torch.utils.data import DataLoader
 from accelerate import Accelerator
 from transformers import get_cosine_schedule_with_warmup
 
-from src.model import VAEEncoder, VAEDecoder, Discriminator
+from src.model import VAEEncoder, VAEDecoder
 from src.data import FaceDataset
 from src.config import VAEConfig
 
@@ -14,13 +14,12 @@ from src.config import VAEConfig
 accelerator = Accelerator()
 
 
-def save_model(encoder, decoder, discriminator, config):
+def save_model(encoder, decoder, config):
     save_model.num += 1
     torch.save(
         {
             "encoder": accelerator.get_state_dict(encoder),
-            "decoder": accelerator.get_state_dict(decoder),
-            "discriminator": accelerator.get_state_dict(discriminator)
+            "decoder": accelerator.get_state_dict(decoder)
         },
         f"experiments/{config.exp_name}/checkpoint_{save_model.num:02}.pt"
     )
@@ -30,56 +29,41 @@ save_model.num = 0
 
 
 def train(
-        encoder: VAEEncoder, decoder: VAEDecoder, discriminator: Discriminator,
+        encoder: VAEEncoder, decoder: VAEDecoder,
         dataloader: DataLoader,
         config: VAEConfig
 ):
-    vae_opt = torch.optim.Adam(
+    opt = torch.optim.Adam(
         list(encoder.parameters()) + list(decoder.parameters()),
         lr=config.lr
     )
-    vae_lr_scheduler = get_cosine_schedule_with_warmup(
-        vae_opt,
+    lr_scheduler = get_cosine_schedule_with_warmup(
+        opt,
         num_warmup_steps=0,
         num_training_steps=config.num_epochs * len(dataloader)
     )
 
-    disc_opt = torch.optim.Adam(
-        discriminator.parameters(), lr=config.lr
-    )
-    disc_lr_scheduler = get_cosine_schedule_with_warmup(
-        disc_opt,
-        num_warmup_steps=0,
-        num_training_steps=config.num_epochs * len(dataloader)
-    )
-
-    encoder, decoder, discriminator, dataloader, vae_opt, disc_opt, vae_lr_scheduler, disc_lr_scheduler = accelerator.prepare(
-        encoder, decoder, discriminator, dataloader, vae_opt, disc_opt, vae_lr_scheduler, disc_lr_scheduler
+    encoder, decoder, dataloader, opt, lr_scheduler = accelerator.prepare(
+        encoder, decoder, dataloader, opt, lr_scheduler
     )
 
     encoder.train()
     decoder.train()
-    discriminator.train()
     for epoch in range(config.num_epochs):
         print(f"EPOCH {epoch + 1} / {config.num_epochs}")
 
         for i, image in enumerate(dataloader):
-            vae_opt.zero_grad()
+            opt.zero_grad()
 
             with accelerator.autocast():
                 dist = encoder(image)
                 z = dist.sample()
                 pred = decoder(z)
 
-                pred_fake = discriminator(pred)
-
                 recon_loss = F.mse_loss(pred, image)
                 kl_loss = dist.kl.mean()
-                adv_loss = F.binary_cross_entropy_with_logits(
-                    pred_fake, torch.ones_like(pred_fake)
-                )
 
-                vae_loss = recon_loss + config.kl_weight * kl_loss + config.adv_weight * adv_loss
+                vae_loss = recon_loss + config.kl_weight * kl_loss
 
             accelerator.backward(vae_loss)
             if config.clip_grad:
@@ -88,41 +72,18 @@ def train(
                     config.clip_grad
                 )
 
-            vae_opt.step()
-            vae_lr_scheduler.step()
-
-            disc_opt.zero_grad()
-
-            with accelerator.autocast():
-                pred_fake = discriminator(pred.detach())
-                pred_real = discriminator(image)
-
-                disc_loss = (
-                    F.binary_cross_entropy_with_logits(
-                        pred_fake, torch.zeros_like(pred_fake)
-                    ) +
-                    F.binary_cross_entropy_with_logits(
-                        pred_real, torch.ones_like(pred_real)
-                    )
-                )
-
-            accelerator.backward(disc_loss)
-            if config.clip_grad:
-                accelerator.clip_grad_norm_(discriminator.parameters(), config.clip_grad)
-
-            disc_opt.step()
-            disc_lr_scheduler.step()
+            opt.step()
+            lr_scheduler.step()
 
             if i % config.log_interval == 0:
                 print(f"{i} / {len(dataloader)} iters.\t"
                       f"Recon Loss: {recon_loss.item():.4f}\t"
-                      f"KL Loss: {kl_loss.item():.4f}\t"
-                      f"Adv Loss: {adv_loss.item():4f}")
+                      f"KL Loss: {kl_loss.item():.4f}\t")
 
             if i > 0 and i % config.save_interval == 0:
-                save_model(encoder, decoder, discriminator, config)
+                save_model(encoder, decoder, config)
 
-        save_model(encoder, decoder, discriminator, config)
+        save_model(encoder, decoder, config)
 
 
 if __name__ == "__main__":
@@ -147,12 +108,6 @@ if __name__ == "__main__":
         dims=config.model.dims,
         depths=config.model.depths
     )
-    discriminator = Discriminator(
-        in_channels=3,
-        dims=config.discriminator.dims,
-        depths=config.discriminator.depths,
-        patch_size=config.discriminator.patch_size
-    )
 
     dataloader = DataLoader(
         dataset,
@@ -163,4 +118,4 @@ if __name__ == "__main__":
 
     config.save()
 
-    train(encoder, decoder, discriminator, dataloader, config)
+    train(encoder, decoder, dataloader, config)
