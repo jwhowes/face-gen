03765996bc886dcd77865de2cdcf242afbb66c00
jwhowes <howesjacob8@gmail.com>
2024-12-26 18:28:38 +0000

    foobar

--- src/train.py
+++ src/train.py
@@ -57,6 +57,8 @@ def train(
                 total_metrics[j] += m
 
             if accelerator.is_main_process and i % config.log_interval == 0:
-                print(f"{i} / {len(dataloader)} iters.\t{"\t".join([f'{k}: {v:.4f}' for k, v in zip(config.metrics, loss['metrics'])])}")
+                print(f"{i} / {len(dataloader)} iters.\t{'\t'.join(
+                    [f'{k}: {v:.4f}' for k, v in zip(config.metrics, loss['metrics'])]
+                )}")
 
         config.log(model, *[m / len(dataloader) for m in total_metrics])
